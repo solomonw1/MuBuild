db148c59d2629dce2dfc445b50af71d72afe5299
Solomon Wood <solomon.m.wood@outlook.com>
2021-11-29 06:42:04 +0000

    Add a setup for Windows

---
+++ install/install.bat
@@ -0,0 +1,10 @@
+mkdir C:\bin
+
+REM Copy our files
+copy install\mubuild.bat C:\bin\mubuild.bat
+copy mubuild.py C:\bin\mubuild.py
+IF EXIST C:\bin\donepath exit
+
+REM add to path
+setx path "C:\bin;%path%"
+echo DonePath >> C:\bin\donepath
